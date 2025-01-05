6fabb7a6172a24fdf45b48348729e9e14e79500d
Kaspar Manz <code@xeophin.net>
2025-01-05 21:04:31 +0100

    chore: remove cron stuff

--- src/cron.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { CronJob } from "cron";
-import { main } from "./main.js";
-
-console.log("Starting cron");
-new CronJob("0 * * * *", () => {
-  main();
-}).start();
